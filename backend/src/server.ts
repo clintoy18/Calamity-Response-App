72485490bbf65d2105e907d471686269ab5c26c5
AdrianeDriane <dilaoaparre@gmail.com>
2025-10-04 14:01:18 +0800

    feat: cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,13 @@ import { connectDB } from "./config/db";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Mount routes
