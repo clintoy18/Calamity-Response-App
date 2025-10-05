ac9c23ac14bd21d9ccb6a5d51cbca96f6c59f1fb
clintoy18 <aicsalonzo@gmail.com>
2025-10-05 22:38:17 +0800

    comment out

--- frontend/src/hooks/queries/userEmergency.ts
+++ frontend/src/hooks/queries/userEmergency.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import api from "../../services/authService";
+// import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+// import api from "../../services/authService";
 
 
